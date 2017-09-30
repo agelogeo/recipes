a44a8efb04b5c8227c72dbf6c16d7d76c42b1901
Giorgos Angelopoulos <agelo1995@gmail.com>
2017-09-30 18:23:28 +0300

    signup first process

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,43 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import {Component, ViewChild} from '@angular/core';
+import {MenuController, NavController, Platform} from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import {TabsPage} from "../pages/tabs/tabs";
+import {SigninPage} from "../pages/signin/signin";
+import {SignupPage} from "../pages/signup/signup";
+import firebase from 'firebase';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage:any = TabsPage;
+  signinPage = SigninPage;
+  signupPage = SignupPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  @ViewChild('content') nav: NavController;
+
+  constructor(private menuCtrl: MenuController,platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+    firebase.initializeApp({
+      apiKey: "AIzaSyB5uG11zi8hOZ98oSbiGe5xO-3Mjsl5Mis",
+      authDomain: "ionic2-recipes-37d19.firebaseapp.com"
+    });
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+  }
+
+  openPage(page: any){
+    this.nav.setRoot(page);
+    this.menuCtrl.close();
+  }
+
+  onLogout(){
+
   }
 }
 
